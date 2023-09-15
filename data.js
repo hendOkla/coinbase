77c0378a4160fd1edddf142d8571aa118d417614
Hendokla99 <hendprog@gmail.com>
2023-09-15 20:50:26 +0300

    update15:9

--- data.js
+++ data.js
@@ -1,44 +1,10 @@
 export const products = [
     {
         id: 1,
-        name: "Small Black Chair",
+        name: "6 figure earner",
         price: 10,
         currency: "USD",
-        description: "Soft Chair, Best value for Money"
+        description: "Standard 6 figure earner plan"
     },
-    {
-        id: 2,
-        name: "Spoon",
-        price: 2,
-        currency: "USDT",
-        description: "Spoon gets the best value for Money"
-    },
-    {
-        id: 3,
-        name: "Small Table",
-        price: 10,
-        currency: "USDC",
-        description: "Soft Table, Best value for Money"
-    },
-    {
-        id: 4,
-        name: "Silver",
-        price: 0.2,
-        currency: "ETH",
-        description: "Silver spoon set"
-    },
-    {
-        id: 5,
-        name: "Silver Cup",
-        price: 0.03,
-        currency: "BTC",
-        description: "Cup for the big men"
-    },
-    {
-        id: 6,
-        name: "Basic Cup",
-        price: 2000,
-        currency: "DOGE",
-        description: "Best value for Money"
-    }
+
 ]
