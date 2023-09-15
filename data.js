072604100a35e60fb9004c739b7344740611b4b9
Hendokla99 <hendprog@gmail.com>
2023-09-15 20:54:58 +0300

    update 8:54

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 1,
         name: "6 figure earner",
-        price: 10,
+        price: 1,
         currency: "USD",
         description: "Standard 6 figure earner plan"
     },
